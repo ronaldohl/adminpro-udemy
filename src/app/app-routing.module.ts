8278334751e6b202970e7f1e381c2d3346209962
ronalucard <rona-lucard@outlook.com>
2019-08-29 20:51:49 -0500

    Primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register.component';
+
+
+const routes: Routes = [
+  {
+    path: '', 
+    component: PagesComponent,
+    children: [
+      {path: 'graficas1', component: Graficas1Component},
+      {path: 'dashboard', component: DashboardComponent},
+      {path: 'progress', component: ProgressComponent},
+      {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+    ]
+  },
+  {path: 'register', component:RegisterComponent },
+  {path: 'login', component: LoginComponent},
+  {path: '**', component: NopagefoundComponent},
+  // {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+
+  
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
