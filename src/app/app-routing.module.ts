256680508e5a9601ae5753f417f00a75e20442d7
ronalucard <rona-lucard@outlook.com>
2019-08-31 11:15:16 -0500

    Modificaciones en los modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,28 @@
-import { NgModule } from '@angular/core';
+// import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+// import { DashboardComponent } from './pages/dashboard/dashboard.component';
+// import { ProgressComponent } from './pages/progress/progress.component';
+// import { Graficas1Component } from './pages/graficas1/graficas1.component';
+// import { PagesComponent } from './pages/pages.component';
 
 
-const routes: Routes = [
-  {
-    path: '', 
-    component: PagesComponent,
-    children: [
-      {path: 'graficas1', component: Graficas1Component},
-      {path: 'dashboard', component: DashboardComponent},
-      {path: 'progress', component: ProgressComponent},
-      {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-    ]
-  },
+const APP_ROUTES: Routes = [
+  //{-- Se movio al pages.routes.ts --}
+  // {
+  //   path: '', 
+  //   component: PagesComponent,
+  //   children: [
+  //     {path: 'graficas1', component: Graficas1Component},
+  //     {path: 'dashboard', component: DashboardComponent},
+  //     {path: 'progress', component: ProgressComponent},
+  //     {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  //   ]
+  // },
   {path: 'register', component:RegisterComponent },
   {path: 'login', component: LoginComponent},
   {path: '**', component: NopagefoundComponent},
@@ -31,8 +32,5 @@ const routes: Routes = [
 
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes, {useHash:true})],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+
+export const AppRoutingModule = RouterModule.forRoot(APP_ROUTES,{useHash:true});
